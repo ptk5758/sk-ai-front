f952cffdd045157d453cdfbe887748e93d0772ad
ptk5758 <ptk725739@gmail.com>
2026-05-09 01:05:57 +0900

    [feature] Agent 예시

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,63 @@
 import { useActionState } from 'react'
 import Title from './components/Title'
-async function reducerAction(prev: { foo: string }, formData: FormData) {
-    const value = await new Promise((resolve, reject) => {
-        setTimeout(() => resolve(formData.get('message')), 1000)
-    })
+
+type ChatItem = {
+    message: string
+    owner: 'AI' | 'HUMAN'
+}
+
+type ChatState = {
+    chatList: ChatItem[]
+}
+
+function getString(formDataEntryValue: FormDataEntryValue | null): string {
+    return typeof formDataEntryValue === 'string' ? formDataEntryValue : ''
+}
+
+function chatRequest(message: string) {
+    return fetch('http://127.0.0.1:8000/ingredient/agent', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+    }).then((res) => res.json())
+}
+
+async function formActionReducer(prev: ChatState, formData: FormData): Promise<ChatState> {
+    const message = getString(formData.get('message')) ?? ''
+    const response = await chatRequest(message)
+    const messages: { id: string; content: string; [key: string]: any }[] = response.messages
     return {
         ...prev,
-        foo: prev.foo + value,
+        chatList: [...prev.chatList, ...messages.map<ChatItem>((item) => ({ message: item.content, owner: 'HUMAN' }))],
     }
+    // console.log(response)
+    // return {
+    //     ...prev,
+    //     chatList: [...prev.chatList, { message: message, owner: 'HUMAN' }],
+    // }
 }
 function App() {
-    const [state, action, pending] = useActionState(reducerAction, { foo: '' })
+    const [state, action, pending] = useActionState(formActionReducer, { chatList: [] })
+
     return (
         <div>
             <Title title="냉장고 관리 Agent" />
-            <h2>{pending}</h2>
-            <h3>{state.foo}</h3>
+            <ul>
+                {state.chatList.length > 0 &&
+                    state.chatList.map((chat, index) => {
+                        return (
+                            <li key={index}>
+                                {chat.owner}:{chat.message}
+                            </li>
+                        )
+                    })}
+            </ul>
             <form action={action}>
-                {pending && <p>전송중...</p>}
                 <input name="message" />
-                <button type="submit">전송</button>
+                <button>전송</button>
+                {pending && <p>전송중..</p>}
             </form>
         </div>
     )
